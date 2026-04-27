c0e52d5f3b6963693818ba62c0b626c89a79ab43
Paul Bianciardi <paul.bianciardi@elastic.co>
2026-04-27 11:08:26 +0100

    Change exporter just to Embrace

--- src/frontend/utils/telemetry/EmbraceTracer.ts
+++ src/frontend/utils/telemetry/EmbraceTracer.ts
@@ -2,13 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { initSDK } from '@embrace-io/web-sdk';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { SessionIdProcessor } from './SessionIdProcessor';
 
-const {
-  NEXT_PUBLIC_EMBRACE_APP_ID: EMBRACE_APP_ID = '',
-  NEXT_PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT = '',
-} = typeof window !== 'undefined' ? window.ENV : {};
+const { NEXT_PUBLIC_EMBRACE_APP_ID: EMBRACE_APP_ID = '' } =
+  typeof window !== 'undefined' ? window.ENV : {};
 
 export const initEmbrace = () => {
   if (typeof window === 'undefined') return;
@@ -16,11 +13,6 @@ export const initEmbrace = () => {
   initSDK({
     appID: EMBRACE_APP_ID,
     appVersion: process.env.NEXT_PUBLIC_OTEL_SERVICE_NAME ?? 'frontend',
-    spanExporters: [
-      new OTLPTraceExporter({
-        url: NEXT_PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT || 'http://localhost:4318/v1/traces',
-      }),
-    ],
     spanProcessors: [new SessionIdProcessor()],
   });
 };
