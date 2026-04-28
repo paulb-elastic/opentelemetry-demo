f63d97e681085190ffdfc764414d0542d7460db8
Paul Bianciardi <paul.bianciardi@elastic.co>
2026-04-28 12:48:20 +0100

    Update environment

--- src/frontend/utils/telemetry/EmbraceTracer.ts
+++ src/frontend/utils/telemetry/EmbraceTracer.ts
@@ -2,10 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { initSDK } from '@embrace-io/web-sdk';
+import { resourceFromAttributes } from '@opentelemetry/resources';
 import { SessionIdProcessor } from './SessionIdProcessor';
 
-const { NEXT_PUBLIC_EMBRACE_APP_ID: EMBRACE_APP_ID = '' } =
-  typeof window !== 'undefined' ? window.ENV : {};
+const {
+  NEXT_PUBLIC_EMBRACE_APP_ID: EMBRACE_APP_ID = '',
+  NEXT_PUBLIC_DEPLOYMENT_ENVIRONMENT = '',
+} = typeof window !== 'undefined' ? window.ENV : {};
 
 export const initEmbrace = () => {
   if (typeof window === 'undefined') return;
@@ -13,6 +16,9 @@ export const initEmbrace = () => {
   initSDK({
     appID: EMBRACE_APP_ID,
     appVersion: process.env.NEXT_PUBLIC_OTEL_SERVICE_NAME ?? 'frontend',
+    resource: resourceFromAttributes({
+      'deployment.environment': NEXT_PUBLIC_DEPLOYMENT_ENVIRONMENT,
+    }),
     spanProcessors: [new SessionIdProcessor()],
   });
 };
